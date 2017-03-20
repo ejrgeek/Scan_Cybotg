9a295cc16d3042899cc66b93370e75488b0ea48a
ejr_geek <ejrflaut@hotmail.com>
2017-03-20 20:31:04 -0300

    Scans Mais Bonitinhos
    Scans Versão 2.0

---
+++ IpCy.py
@@ -0,0 +1,19 @@
+from socket import *
+
+def PegaIp(d,e):
+	d = gethostbyname(e)
+	print("\033[1;31mIP > \033[0;0m",d)
+
+print("""\033[1;34m
+██╗██████╗          ██████╗██╗   ██╗
+██║██╔══██╗        ██╔════╝╚██╗ ██╔╝
+██║██████╔╝        ██║      ╚████╔╝ 
+██║██╔═══╝         ██║       ╚██╔╝  
+██║██║     ███████╗╚██████╗   ██║   
+╚═╝╚═╝     ╚══════╝ ╚═════╝   ╚═╝   
+Coder > ejr_geek
+GitHub > github.com/ejrgeek | Ip_Cy v2.0
+\033[0;0m""")
+dominio = input("\033[1;32mURL > \033[0;0m")
+PegaIp(dominio, dominio)
+
--- ScanFullCy.py
+++ ScanFullCy.py
@@ -15,7 +15,7 @@ print("""\033[1;31m
       ░  ░ ░            ░  ░         ░ ░ ░       ░ ░     
          ░                             ░         ░ ░     
 Coder > ejr_geek
-GitHub > github.com/ejrgeek | ScanCy v2.0
+GitHub > github.com/ejrgeek | ScanFullCy v2.0
 \033[0;0m""")
 ip=input("\033[1;34mIP/HOST ou URL > \033[0;0m")
 
@@ -26,3 +26,5 @@ for porta in range(0,65536):
     if conexao == 0:
         print ("\033[1;32m[☢]",porta,"> Porta Aberta\033[0;0m")
         time.sleep(0.2)
+    else:
+        print ("\033[1;31m",porta,"> Porta Fechada\033[0;0m")
