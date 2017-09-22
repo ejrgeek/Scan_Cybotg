5db96ab2a7d8e1b053ca8849076fdba08edcd1ee
Erlon Jr <ejrflaut@hotmail.com>
2017-09-22 16:21:42 -0300

    ScanFullCy.py Versao 3.0

--- ScanFullCy.py
+++ ScanFullCy.py
@@ -15,11 +15,11 @@ print("""\033[1;31m
       ░  ░ ░            ░  ░         ░ ░ ░       ░ ░     
          ░                             ░         ░ ░     
 Coder > ejr_geek
-GitHub > github.com/ejrgeek | ScanFullCy v2.0
+GitHub > github.com/ejrgeek | ScanFullCy v3.0
 \033[0;0m""")
 ip=input("\033[1;34mIP/HOST ou URL > \033[0;0m")
 
-for porta in range(0,65536):
+for porta in range(0,65535):
     cliente=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliente.settimeout(0.5)
     conexao=cliente.connect_ex((ip,porta))
